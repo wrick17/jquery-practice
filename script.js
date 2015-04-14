bf3354d73a1b78bc918321978e3d8326865f323a
Pratyush Poddar <wrick17@gmail.com>
2015-04-15 01:23:41 +0530

    code improved add new functionality to remove items from selected items add tabbed interface

--- script.js
+++ script.js
@@ -1,99 +1,135 @@
 (function () {
 
-  var totalMains = $('.table.mains tbody tr td input:checkbox').length,
-      selectedMains = 0,
-      totalOptional = $('.table.optional tbody tr td input:checkbox').length,
-      selectedOptional = 0;
-      subName = null,
-      entry = null;
+  var totalMains = $('.mains tbody tr td input:checkbox').length,
+  selectedMains = 0,
+  totalOptional = $('.optional tbody tr td input:checkbox').length,
+  selectedOptional = 0;
+  subName = null,
+  entry = null,
+  body = $('body'),
+  tableName = null,
+  tabs = $('.tab'),
+  panes = $('.tab-pane');
 
-  $('.table.mains tbody tr td input:checkbox').on('click', function(event) {
+  init();
 
-    selectedMains = $('.table.mains tbody tr td input:checkbox:checked').length;
+  function init () {
 
-    // console.log($(this).is(':checked'));
+    body.on('click', '.tab', function(event) {
+      tableName = '.' + $(this).text().toLowerCase();
+      tabs.removeClass('active');
+      panes.removeClass('active');
+      $(this).addClass('active');
+      $(tableName).addClass('active');
+    });
 
-    subName = $(this).parent().text();
-    if ($(this).is(':checked')) {
+    body.on('click', '.main-sub', function(event) {
 
-      $('.table.selected tbody tr td label:contains("No Subjects Selected")').parents('tr').remove();
-      entry = '<tr>'+
-                '<td>' +
-                    '<i class="glyphicon glyphicon-circle-arrow-up move-up"></i>' +
-                    '<i class="glyphicon glyphicon-circle-arrow-down move-down"></i>' +
-                    '<label>'+subName+'</label>' +
-                  '</td>'+
-              '</tr>';
-      $('.table.selected tbody').append(entry);
-    }
-    else {
-      $('.table.selected tbody tr td label:contains('+subName+')').parents('tr').remove();
-      if ($('.table.selected tbody tr').length === 0) {
+      subName = $(this).parent().text();
+      if ($(this).is(':checked')) {
+
+        $('.selected tbody tr td label:contains("No Subjects Selected")').parents('tr').remove();
         entry = '<tr>'+
-                  '<td><label>No Subjects Selected</label></td>'+
-                '</tr>';
-        $('.table.selected tbody').append(entry);
+        '<td>' +
+        '<i class="glyphicon glyphicon-circle-arrow-up move-up"></i>' +
+        '<i class="glyphicon glyphicon-circle-arrow-down move-down"></i>' +
+        '<label>'+subName+'</label>' +
+        '<i class="glyphicon glyphicon-remove-sign remove"></i>' +
+        '</td>'+
+        '</tr>';
+        $('.selected tbody').append(entry);
       }
-    }
+      else {
+        $('.selected tbody tr td label:contains('+subName+')').parents('tr').remove();
+        checkSelected();
+      }
 
+      checkMains();
+    });
+
+    body.on('click', '.optional-sub', function(event) {
+
+      subName = $(this).parent().text();
+      if ($(this).is(':checked')) {
+        $('.selected tbody tr td label:contains("No Subjects Selected")').parents('tr').remove();
+        entry = '<tr>'+
+        '<td>' +
+        '<i class="glyphicon glyphicon-circle-arrow-up move-up"></i>' +
+        '<i class="glyphicon glyphicon-circle-arrow-down move-down"></i>' +
+        '<label>'+subName+'</label>' +
+        '<i class="glyphicon glyphicon-remove-sign remove"></i>' +
+        '</td>'+
+        '</tr>';
+        $('.selected tbody').append(entry);
+      }
+      else {
+        $('.selected tbody tr td label:contains('+subName+')').parents('tr').remove();
+        checkSelected();
+      }
+
+      checkOptional();
+    });
+
+    body.on('click', '.move-up', function (event) {
+
+      var el = $(this).parents('tr');
+      if (el.index() > 0) {
+        el.clone().insertBefore(el.siblings()[el.index()-1]);
+        el.remove();
+      }
+    });
+
+    body.on('click', '.move-down', function (event) {
+
+      var el = $(this).parents('tr');
+      if (el.index() < el.siblings().length) {
+        el.clone().insertAfter(el.siblings()[el.index()]);
+        el.remove();
+      }
+    });
+
+    body.on('click', '.remove', function(event) {
+      var subject = $(this).siblings('label').text();
+      var el = $('.tab-pane:not(.selected) label:contains(' + subject + ')');
+      el.find('input').attr('checked', false);
+      $(this).parents('tr').remove();
+      checkSelected();
+      checkMains();
+      checkOptional();
+    });
+
+  }
+
+  function checkMains() {
+
+    selectedMains = $('.mains tbody tr td input:checkbox:checked').length;
     if (selectedMains === 3) {
-      $('.table.mains tbody tr td input:checkbox:not(:checked)').attr('disabled', true);;
+      $('.mains tbody tr td input:checkbox:not(:checked)').attr('disabled', true);
     }
     else {
-     $('.table.mains tbody tr td input:checkbox:not(:checked)').attr('disabled', false);;
+      $('.mains tbody tr td input:checkbox:not(:checked)').attr('disabled', false);
     }
-  });
+  }
 
-  $('.table.optional tbody tr td input:checkbox').on('click', function(event) {
+  function checkOptional() {
 
-    selectedOptional = $('.table.optional tbody tr td input:checkbox:checked').length;
-
-    subName = $(this).parent().text();
-    if ($(this).is(':checked')) {
-      $('.table.selected tbody tr td label:contains("No Subjects Selected")').parents('tr').remove();
-      entry = '<tr>'+
-                '<td>' +
-                    '<i class="glyphicon glyphicon-circle-arrow-up move-up"></i>' +
-                    '<i class="glyphicon glyphicon-circle-arrow-down move-down"></i>' +
-                    '<label>'+subName+'</label>' +
-                  '</td>'+
-              '</tr>';
-      $('.table.selected tbody').append(entry);
-    }
-    else {
-      $('.table.selected tbody tr td label:contains('+subName+')').parents('tr').remove();
-      if ($('.table.selected tbody tr').length === 0) {
-        entry = '<tr>'+
-                  '<td><label>No Subjects Selected</label></td>'+
-                '</tr>';
-        $('.table.selected tbody').append(entry);
-      }
-    }
-
+    selectedOptional = $('.optional tbody tr td input:checkbox:checked').length;
     if (selectedOptional === 2) {
-      $('.table.optional tbody tr td input:checkbox:not(:checked)').attr('disabled', true);;
+      $('.optional tbody tr td input:checkbox:not(:checked)').attr('disabled', true);
     }
     else {
-     $('.table.optional tbody tr td input:checkbox:not(:checked)').attr('disabled', false);;
+      $('.optional tbody tr td input:checkbox:not(:checked)').attr('disabled', false);
     }
-  });
+  }
 
-  $('body').on('click', 'i.move-up', function (event) {
-
-    var el = $(this).parents('tr');
-    if (el.index() > 0) {
-      el.clone().insertBefore(el.siblings()[el.index()-1]);
-      el.remove();
+  function checkSelected() {
+    if ($('.selected tbody tr').length === 0) {
+      entry = '<tr>'+
+      '<td><label>No Subjects Selected</label></td>'+
+      '</tr>';
+      $('.selected tbody').append(entry);
     }
-  });
+  }
 
-  $('body').on('click', 'i.move-down', function (event) {
-
-    var el = $(this).parents('tr');
-    if (el.index() < el.siblings().length) {
-      el.clone().insertAfter(el.siblings()[el.index()]);
-      el.remove();
-    }
-  });
 
 })();
